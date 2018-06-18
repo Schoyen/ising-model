c85360b6c5ba5853700f8822de362bd0f85906d6
Mint-kontor <oyvindschoyen@gmail.com>
2018-06-18 15:01:02 +0200

    Compute all exact energies in one go

--- compare.py
+++ compare.py
@@ -10,21 +10,18 @@ def exact_energy(num_spins, temperature):
 
 
 temperatures = np.linspace(0.1, 3.0, 101)
-num_cycles = int(1e5)
+num_cycles = int(1e4)
 
 num_spins = 40
-sampled_energies = []
-exact_energies = []
+sampled_energies = np.zeros_like(temperatures)
 
-for temperature in tqdm.tqdm(temperatures):
-    exact_energies.append(exact_energy(num_spins, temperature))
-    sampled_energies.append(
-        sample_ising(
-            np.random.choice([-1, 1], num_spins), num_cycles, temperature
-        )
-    )
+for i, temperature in enumerate(tqdm.tqdm(temperatures)):
+    grid = np.random.choice([-1, 1], num_spins)
+    energy = sample_ising(grid, num_cycles, temperature)
 
-plt.plot(temperatures, exact_energies, label="Exact")
+    sampled_energies[i] = energy
+
+plt.plot(temperatures, exact_energy(num_spins, temperatures), label="Exact")
 plt.plot(temperatures, sampled_energies, label="Sampled")
 plt.legend(loc="best")
 plt.show()
